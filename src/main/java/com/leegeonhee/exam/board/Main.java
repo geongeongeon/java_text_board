d0188a2750735b92be261cf245d7d9fd9b182fe1
geongeongeon <leegeonhee0204@naver.com>
2024-04-16 21:55:10 +0900

    List에 데이터 담기

--- src/main/java/com/leegeonhee/exam/board/Main.java
+++ src/main/java/com/leegeonhee/exam/board/Main.java
@@ -1,14 +1,33 @@
 package com.leegeonhee.exam.board;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
+    static void addTestData(List<Article> dataList){
+        dataList.add(new Article(1, "제목1", "내용1"));
+        dataList.add(new Article(2, "제목2", "내용2"));
+        dataList.add(new Article(3, "제목3", "내용3"));
+        dataList.add(new Article(4, "제목4", "내용4"));
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int articleLastId = 0;
+
+        Article data = null;
+        List<Article> dataList = new ArrayList<>();
+
+        addTestData(dataList);
+
+        if(dataList.size() > 0) {
+            articleLastId = dataList.get(dataList.size()-1).id;
+            data = dataList.get(dataList.size()-1);
+        }
+
         System.out.println("== 텍스트 게시판 v 0.1 ==");
         System.out.println("프로그램 시작");
-
-        int articleLastId = 0;
 
         while(true) {
             System.out.printf("명령)");
@@ -25,12 +44,31 @@ public class Main {
                 System.out.printf("내용)");
                 String body = scanner.nextLine();
 
-                Article data = new Article();
-                data.storage(id, title, body);
+                data = new Article(id, title, body);
+                dataList.add(data);
 
                 System.out.println(data);
-
                 System.out.printf("%d번 게시물이 등록되었습니다.\n", id);
+            }
+            else if(cmd.equals("detail")) {
+                System.out.println("== 게시물 상세보기 ==");
+
+                if(dataList.isEmpty()) {
+                    System.out.println("게시물이 없습니다.");
+                } else {
+                    System.out.println("id : " + data.id);
+                    System.out.println("title : " + data.title);
+                    System.out.println("body : " + data.body);
+                }
+            }
+            else if(cmd.equals("list")) {
+                System.out.println("== 게시물 리스트 ==");
+                System.out.println("id | title");
+
+                for(int i = dataList.size()-1; i >= 0; i--){
+                    Article article = dataList.get(i);
+                    System.out.printf(" %d  | %s\n", article.id, article.title);
+                }
             }
             else if(cmd.equals("exit")) {
                 System.out.println("게시판을 종료합니다.");
@@ -45,13 +83,15 @@ class Article {
     int id;
     String title;
     String body;
-    void storage(int id, String title, String body) {
+
+    public Article (int id, String title, String body) {
         this.id = id;
         this.title = title;
         this.body = body;
     }
+
     @Override
     public String toString() {
-        return String.format("id : %d, title : %s, body : %s", id, title, body);
+        return String.format("id : %d, title : \"%s\", body : \"%s\"", id, title, body);
     }
 }
